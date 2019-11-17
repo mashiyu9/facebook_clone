438ed08de81c8597fbdf914e621154f25c09d2dc
yu shimamura <mashiyu0914@gmail.com>
2019-11-17 18:03:53 +0900

    add : sessionsコントローラー
    登録フォーム作成,バリデーションに失敗した時まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :pictures
-  resources :users, only: [:new]
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
