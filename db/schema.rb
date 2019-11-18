15f32fb8548cde3a6012225867a239ee17566d8d
yu shimamura <mashiyu0914@gmail.com>
2019-11-18 13:23:24 +0900

    add : 画像投稿機能の作成
    画像投稿機能をつける。機能面ではとりあえず完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_070900) do
+ActiveRecord::Schema.define(version: 2019_11_18_035159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_17_070900) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
     t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
