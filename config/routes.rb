131b6161ea88b2de571f8c8600ef50546eebd9c0
yu shimamura <mashiyu0914@gmail.com>
2019-11-18 11:37:09 +0900

    add : 少しcommitするのを忘れていた。pictureコントローラーの修正
    commit忘れは反省 pictureを新しく作った時のIDが入ってないエラーの解決

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :pictures
+  resources :pictures do
+    collection do 
+      patch :confirm
+      post :confirm
+    end
+    member do 
+      patch :confirm 
+    end    
+  end
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
